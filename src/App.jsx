3099ecf5ba18e81d132a5a91f69e4f53ed67510d
martsDev <martssilvapro@gmail.com>
2022-09-30 13:53:40 -0300

    add basic style and custom fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import './App.css';
 const App = () => {
   return (
     <section className="main-section">
-
+      <section className='header-section'>
+        <h1>splitter</h1>
+      </section>
+      <section className='calculator-section'>
+        Calculator
+      </section>
     </section>
   );
 };
