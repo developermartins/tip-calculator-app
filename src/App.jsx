e5e107cd7761bab5c46063d2a091912cb40081cd
martsDev <martssilvapro@gmail.com>
2022-09-30 14:18:33 -0300

    add basic HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,37 @@ const App = () => {
   return (
     <section className="main-section">
       <section className='header-section'>
-        <h1>splitter</h1>
+        <h1>spli<br />tter</h1>
       </section>
       <section className='calculator-section'>
-        Calculator
+        <span>Bill</span>
+        <input type="text" />
+        <span>Select Tip %</span>
+        <section className='buttons-section'>
+          <button>5%</button>
+          <button>10%</button>
+          <button>15%</button>
+          <button>25%</button>
+          <button>50%</button>
+          <button>Custom</button>
+        </section>
+        <span>Number of people</span>
+        <input type="text" />
+        <section className="result-section">
+          <div className='amount-container'>
+            <p>Tip Amount <br />
+              <span>/ person</span>
+            </p>
+            <span>$4.27</span>
+          </div>
+          <div className='total-container'>
+            <p>Total <br />
+              <span>/ person</span>
+              <span>$32.79</span>
+            </p>
+          </div>
+          <button>RESET</button>
+        </section>
       </section>
     </section>
   );
